8a77117b5e01f2be8ab5fe91f77f914bc76b9cda
QianChen <xgdjqc@126.com>
2015-03-26 22:47:37 +0800

    Create the project.

---
+++ QQList/QQList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQList
+//
+//  Created by 钱辰 on 3/26/15.
+//  Copyright (c) 2015 qianchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQList/QQList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQList
+//
+//  Created by 钱辰 on 3/26/15.
+//  Copyright (c) 2015 qianchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
